c2c2e27543644e790fb48ee5fdac8cfb13812c95
Ryan Perry <rperry@mindimage.net>
2016-12-29 18:33:46 -0700

    E

--- sites/all/modules/clay_mod/js/clay_mod.js
+++ sites/all/modules/clay_mod/js/clay_mod.js
@@ -1,13 +1,10 @@
 (function ($) {
-    $(document).ready(function () {
 
-        var phones = [{ "mask": "(###) ###-####"}, { "mask": "(###) ###-##############"}];
-        $('#edit-phone').inputmask({
-            mask: phones,
-            greedy: false,
-            definitions: { '#': { validator: "[0-9]", cardinality: 1}} });
-
-        console.log(phones);
-
+    $('#edit-phone').simpleMask({
+        'mask': ['(###) ###-####'],
+        'nextInput': null
     });
+
+console.log($('#edit-phone'));
+
 })(jQuery);
